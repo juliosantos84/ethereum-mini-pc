72377f37c68c57155b6cf3a0b67290eb54a14f7b
juliosantos84 <julio@getflex.com>
2022-10-22 16:12:28 -0400

    Make /var/lib/ethereum shared.

--- bin/create-directories.sh
+++ bin/create-directories.sh
@@ -8,8 +8,10 @@ echo "Creating subdirectories..." \
 && sudo mkdir -p ${DATA_DIR}/shared
 
 echo "Setting ownership..." \
+&& sudo chown -R :ethereumshared ${DATA_DIR} \
 && sudo chown -R ethereumexecution:ethereumexecution ${DATA_DIR}/execution \
 && sudo chown -R ethereumconsensus:ethereumconsensus ${DATA_DIR}/consensus \
 && sudo chown -R ethereumvalidator:ethereumvalidator ${DATA_DIR}/consensus/validators \
 && sudo chown -R :ethereumshared ${DATA_DIR}/shared \
-&& sudo chmod g+wr ${DATA_DIR}/shared
+&& sudo chmod g+wr ${DATA_DIR}/shared \
+&& sudo chmod g+wr ${DATA_DIR}
