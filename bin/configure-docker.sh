03d82d3ed1aba6e6c799867831b52f682e0b444a
juliosantos84 <julio@getflex.com>
2022-10-22 16:46:14 -0400

    Fix docker install.

--- bin/configure-docker.sh
+++ bin/configure-docker.sh
@@ -1,7 +1,22 @@
 #! /bin/bash
 
-1  sudo groupadd docker
-   sudo usermod -aG docker $USER
-   newgrp docker
-   sudo chgrp docker /var/run/docker.sock
+sudo snap remove docker --purge
+
+sudo apt-get update
+sudo apt-get install     ca-certificates     curl     gnupg     lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo docker run hello-world
+docker run --help
+sudo systemctl start docker
+sudo systemctl status docker
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+sudo chgrp docker /var/run/docker.sock
    
