47c0cd4611dff5e9f92fe28310d853f89bbb4e5e
juliosantos84 <julio@getflex.com>
2023-03-11 12:06:01 -0500

    Upgrade geth and lighthouse.

--- bin/configure-rocketpool.sh
+++ bin/configure-rocketpool.sh
@@ -2,3 +2,9 @@
 
 sudo wget https://github.com/rocket-pool/smartnode-install/releases/latest/download/rocketpool-cli-linux-amd64 -O /bin/rocketpool
 sudo chmod +x /bin/rocketpool
+sudo mkdir -p /var/lib/ethereum/rocketpool
+sudo useradd --no-create-home --shell /bin/false rocketpool
+sudo chown -R rocketpool:rocketpool /var/lib/ethereum/rocketpool
+sudo usermod -a -G rocketpool $USER
+
+ 
