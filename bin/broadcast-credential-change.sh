b93fc874eb3c47d43a16f70f999b27686cc243a5
juliosantos84 <julio@getflex.com>
2023-03-23 20:42:51 -0400

    fix script again.

--- bin/broadcast-credential-change.sh
+++ bin/broadcast-credential-change.sh
@@ -2,5 +2,6 @@
 
 docker run -v $HOME/change-operations.json:/ethdowc/change-operations.json \
 -w /ethdowc \
+--network=host wealdtech/ethdo:latest \
+validator credentials set \
 --connection=http://localhost:5052 \
---network=host wealdtech/ethdo:latest validator credentials set
