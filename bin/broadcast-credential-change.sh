de1b1d7f2026519cb53bc9c2360182f54a5059a5
juliosantos84 <julio@getflex.com>
2023-03-24 21:41:32 -0400

    Update broadcast cred file.

--- bin/broadcast-credential-change.sh
+++ bin/broadcast-credential-change.sh
@@ -1,7 +1,13 @@
 #! /bin/bash
 
-docker run -v $HOME/change-operations.json:/ethdowc/change-operations.json \
--w /ethdowc \
---network=host wealdtech/ethdo:latest \
+docker run --network=host wealdtech/ethdo:latest \
+validator credentials get \
+--connection=http://localhost:5052 \
+--validator=255860
+
+docker run --network=host wealdtech/ethdo:latest \
 validator credentials set \
 --connection=http://localhost:5052 \
+--signed-operations "$(cat $HOME/change-operations.json)"
+
+curl http://localhost:5052/eth/v1/beacon/pool/bls_to_execution_changes
