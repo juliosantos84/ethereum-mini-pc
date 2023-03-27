0cc6c5465f27f9ac5e151ff01abe287f8c052295
juliosantos84 <julio@getflex.com>
2023-03-27 13:34:58 -0400

    Add new lines.

--- bin/broadcast-credential-change.sh
+++ bin/broadcast-credential-change.sh
@@ -1,13 +1,18 @@
 #! /bin/bash
 
+# display current creds
 docker run --network=host wealdtech/ethdo:latest \
 validator credentials get \
 --connection=http://localhost:5052 \
 --validator=255860
 
+echo
+
 docker run --network=host wealdtech/ethdo:latest \
 validator credentials set \
 --connection=http://localhost:5052 \
 --signed-operations "$(cat $HOME/change-operations.json)"
+
+echo 
 
 curl http://localhost:5052/eth/v1/beacon/pool/bls_to_execution_changes
