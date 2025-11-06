82efa244f0d87745b59afafc88b920ac0ea0127c
juliosantos84 <julio.r.santos.jr@gmail.com>
2025-11-06 15:54:04 -0500

    Update script remove-geth-db.sh.

--- bin/remove-geth-db.sh
+++ bin/remove-geth-db.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
-docker compose run -it --rm execution \
-    removedb  --datadir /var/lib/ethereum/execution/
+ docker compose run -it --rm execution \
+    removedb  --datadir /var/lib/ethereum/execution/ \
+    --datadir.ancient /var/lib/ethereum_ext/geth/ancient
